cee91a956f8b47183759b82c0e15ac4392e15a9d
Yamatsu <sorakakeru@users.noreply.github.com>
2023-05-27 17:40:11 +0900

    change 23/5/27
    /about/
    rss.xml

--- renderers.mjs
+++
@@ -1,6 +0,0 @@
-import { h as server_default } from './chunks/astro.127921cf.mjs';
-import 'html-escaper';
-
-const renderers = [Object.assign({"name":"astro:jsx","serverEntrypoint":"astro/jsx/server.js","jsxImportSource":"astro"}, { ssr: server_default }),];
-
-export { renderers };
